6854fea778049d5380c7f77e709d6e258fc49352
luk <740491884@qq.com>
2020-08-24 01:28:27 +0800

    08/24

--- src/main/java/org/code/algorithm/leetcode/TwoPage.java
+++ src/main/java/org/code/algorithm/leetcode/TwoPage.java
@@ -19,27 +19,28 @@ public class TwoPage {
         TwoPage page = new TwoPage();
 
 
-        String s = "catsanddog";
-
-        ListNode head = new ListNode(1);
-
-        ListNode two = new ListNode(2);
-
-        head.next = two;
-
-        ListNode three = new ListNode(3);
-
-        two.next = three;
-
-        three.next = new ListNode(4);
-
-        three.next.next = new ListNode(5);
+//        String s = "catsanddog";
+//
+//        ListNode head = new ListNode(1);
+//
+//        ListNode two = new ListNode(2);
+//
+//        head.next = two;
+//
+//        ListNode three = new ListNode(3);
+//
+//        two.next = three;
+//
+//        three.next = new ListNode(4);
+//
+//        three.next.next = new ListNode(5);
 
 //        List<String> wordDict = Arrays.asList("cat", "cats", "and", "sand", "dog");
 //        page.wordBreakV2(s, wordDict);
 
 //        page.reorderList(head);
-        page.reverseWords("a good   example");
+        int[] nums = new int[]{2, 2, 2, 0, 1};
+        page.findMinInRepeatNumsV2(nums);
     }
 
     /**
@@ -1346,6 +1347,93 @@ public class TwoPage {
             minValue = tmpMin;
         }
         return result;
+
+    }
+
+
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MAX_VALUE;
+        }
+        int left = 0;
+
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+            int mid = left + (right - left) / 2;
+
+
+            if (nums[left] <= nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
+
+    public int findMinV2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MAX_VALUE;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] <= nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+
+            }
+        }
+        return nums[left];
+    }
+
+
+    public int findMinInRepeatNums(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MAX_VALUE;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == nums[right]) {
+                right--;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+
+    }
+
+
+    public int findMinInRepeatNumsV2(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+            int mid = left + (right - left) / 2;
+
+            if (nums[left] < nums[mid]) {
+                left = mid + 1;
+            } else if (nums[left] > nums[mid]) {
+                right = mid;
+            } else {
+                left++;
+            }
+        }
+        return nums[left];
 
     }
 
