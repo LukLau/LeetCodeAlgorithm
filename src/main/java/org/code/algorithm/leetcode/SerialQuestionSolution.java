7e0c49b2c61427aaf6aa387629b4ce943a243ad2
luk <740491884@qq.com>
2020-09-10 01:28:19 +0800

    09/10

--- src/main/java/org/code/algorithm/leetcode/SerialQuestionSolution.java
+++ src/main/java/org/code/algorithm/leetcode/SerialQuestionSolution.java
@@ -1,6 +1,11 @@
 package org.code.algorithm.leetcode;
 
+import org.code.algorithm.datastructe.ListNode;
+
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * @author luk
@@ -10,6 +15,14 @@ public class SerialQuestionSolution {
 
 
     // ---最长无重复子串问题--- //
+
+    public static void main(String[] args) {
+        SerialQuestionSolution solution = new SerialQuestionSolution();
+        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        solution.search(nums, 3);
+    }
+
+    // --- 求中位数 O log(N)算法---- //
 
     /**
      * 3. Longest Substring Without Repeating Characters
@@ -36,7 +49,7 @@ public class SerialQuestionSolution {
 
     }
 
-    // ---O log(N)算法---- //
+    // --正则表达式匹配问题 //
 
     /**
      * 4. Median of Two Sorted Arrays
@@ -49,8 +62,6 @@ public class SerialQuestionSolution {
         return -1;
     }
 
-    // --正则表达式匹配问题 //
-
     /**
      * 10. Regular Expression Matching
      *
@@ -59,10 +70,159 @@ public class SerialQuestionSolution {
      * @return
      */
     public boolean isMatch(String s, String p) {
-        if (s.isEmpty()) {
-            return true;
+        if (p.isEmpty()) {
+            return s.isEmpty();
         }
-        return false;
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;
+
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 1] || dp[i][j - 2] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    // --旋转数组系列问题-- //
+
+    /**
+     * 30. Substring with Concatenation of All Words
+     * todo
+     * answer https://github.com/grandyang/leetcode/issues/30
+     */
+    public List<Integer> findSubstring(String s, String[] words) {
+        return null;
+    }
+
+    /**
+     * 33. Search in Rotated Sorted Array
+     */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[left] < nums[mid]) {
+                if (target < nums[mid] && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+
+
+    /**
+     * 23. Merge k Sorted Lists
+     *
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(lists.length, Comparator.comparingInt(o -> o.val));
+        for (ListNode list : lists) {
+            if (list != null) {
+                priorityQueue.offer(list);
+            }
+        }
+        ListNode root = new ListNode(0);
+        ListNode dummy = root;
+        while (!priorityQueue.isEmpty()) {
+            ListNode poll = priorityQueue.poll();
+
+            dummy.next = poll;
+
+            dummy = dummy.next;
+
+            if (poll.next != null) {
+                priorityQueue.offer(poll.next);
+            }
+        }
+        return root.next;
+    }
+
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        int count = 0;
+        ListNode currentNode = head;
+        while (currentNode != null && count != k) {
+            currentNode = currentNode.next;
+            count++;
+        }
+        if (count == k) {
+            ListNode reverseKGroup = reverseKGroup(currentNode, k);
+            while (k-- > 0) {
+                ListNode tmp = head.next;
+                head.next = reverseKGroup;
+                reverseKGroup = head;
+                head = tmp;
+            }
+            head = reverseKGroup;
+        }
+        return head;
+    }
+
+
+    /**
+     * todo
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroupV2(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode currentNode = head;
+        for (int i = 0; i < k; i++) {
+            if (currentNode == null) {
+                return head;
+            }
+            currentNode = currentNode.next;
+        }
+        ListNode newHead = reverseKGroupV2(currentNode, k);
+
+        while (head != newHead) {
+            ListNode tmp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = tmp;
+        }
+        return newHead;
     }
 
 
