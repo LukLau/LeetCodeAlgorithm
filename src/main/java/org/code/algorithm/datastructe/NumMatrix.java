7f320905386526828773d62ffe86676e80753036
dora <740491884@qq.com>
2020-11-20 17:58:50 +0800

    11/20

---
+++ src/main/java/org/code/algorithm/datastructe/NumMatrix.java
@@ -0,0 +1,27 @@
+package org.code.algorithm.datastructe;
+
+/**
+ * 304 Range Sum Query 2D - Immutable
+ *
+ * @author luk
+ * @date 2020/11/20
+ */
+public class NumMatrix {
+
+    private final int[][] matrix;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int result = 0;
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                result += this.matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+}
--- src/main/java/org/code/algorithm/leetcode/ForPage.java
+++ src/main/java/org/code/algorithm/leetcode/ForPage.java
@@ -1,5 +1,7 @@
 package org.code.algorithm.leetcode;
 
+import org.code.algorithm.datastructe.Point;
+
 import java.util.*;
 
 /**
@@ -73,6 +75,114 @@ public class ForPage {
         }
         return count == 0;
     }
+
+
+    /**
+     * 302 Smallest Rectangle Enclosing Black Pixels
+     *
+     * @param image: a binary matrix with '0' and '1'
+     * @param x:     the location of one of the black pixels
+     * @param y:     the location of one of the black pixels
+     * @return: an integer
+     */
+    public int minArea(char[][] image, int x, int y) {
+        // write your code here
+        int row = image.length;
+        int column = image[0].length;
+        int left = column;
+        int right = 0;
+        int up = row;
+        int bottom = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (image[i][j] == '1') {
+                    left = Math.min(left, j);
+                    right = Math.max(right, j);
+                    up = Math.min(up, i);
+                    bottom = Math.max(bottom, j);
+                }
+            }
+        }
+        return (right - left + 1) * (bottom - up + 1);
+    }
+
+
+    /**
+     * todo
+     *
+     * @param image
+     * @param x
+     * @param y
+     * @return
+     */
+    public int minAreaV2(char[][] image, int x, int y) {
+        int row = image.length;
+        int column = image[0].length;
+        int up = getIndex(image, true, 0, x, 0, column, true);
+        int down = getIndex(image, true, x + 1, row, 0, column, false);
+        int left = getIndex(image, false, 0, y, up, down, true);
+        int right = getIndex(image, false, y + 1, column, up, down, false);
+        return (right - left) * (down - up);
+    }
+
+    private int getIndex(char[][] image, boolean vertx, int i, int j, int low, int high, boolean opt) {
+        while (i < j) {
+            int k = low, mid = (i + j) / 2;
+            while (k < high && (vertx ? image[mid][k] : image[k][mid]) == '0') {
+                ++k;
+            }
+            if (k < high == opt) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i;
+    }
+
+
+    /**
+     * @param n:         An integer
+     * @param m:         An integer
+     * @param operators: an array of point
+     * @return: an integer array
+     */
+    public List<Integer> numIslands2(int n, int m, Point[] operators) {
+        // write your code here
+        int[][] matrix = new int[n][m];
+        LinkedList<Point> deque = new LinkedList<>();
+        for (Point operator : operators) {
+            deque.offer(operator);
+        }
+        int[][] params = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        List<Integer> result = new ArrayList<>();
+        while (!deque.isEmpty()) {
+            Point poll = deque.poll();
+            int x = poll.x;
+            int y = poll.y;
+            int count = 0;
+            if (matrix[x][y] == -1) {
+                result.add(count);
+                continue;
+            }
+            count++;
+            for (int[] param : params) {
+                x = x + param[0];
+                y = y + param[1];
+                if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[x].length || matrix[x][y] == -1) {
+                    continue;
+                }
+                matrix[x][y] = -1;
+                count++;
+            }
+        }
+        return result;
+    }
+
+    private int intervalNumIslands2(int x, int y, int[][] matrix, int count) {
+        return -1;
+    }
+
 
     public static void main(String[] args) {
         ForPage page = new ForPage();
