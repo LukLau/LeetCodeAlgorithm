d5266272d5ac09a31a6c931c0b4d2c9de1499104
luk <740491884@qq.com>
2020-12-16 00:40:34 +0800

    12/16

--- src/main/java/org/code/algorithm/leetcode/SerialQuestionSolution.java
+++ src/main/java/org/code/algorithm/leetcode/SerialQuestionSolution.java
@@ -63,54 +63,6 @@ public class SerialQuestionSolution {
 
     }
 
-    /**
-     * 34. Find First and Last Position of Element in Sorted Array
-     *
-     * @param nums
-     * @param target
-     * @return
-     */
-    public int[] searchRange(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return new int[]{-1, -1};
-        }
-        int[] result = new int[2];
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        if (nums[left] != target) {
-            return new int[]{-1, -1};
-        }
-        result[0] = left;
-        right = nums.length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2 + 1;
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid;
-            }
-        }
-        result[1] = left;
-
-        return result;
-    }
-
-    public int[] searchRangeV2(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return new int[]{-1, -1};
-        }
-        int firstIndex = searchLeftIndex(nums, target, 0, nums.length - 1);
-        return nums;
-    }
-
     public int searchInsert(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return 0;
